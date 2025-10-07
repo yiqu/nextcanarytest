c84895e1ae9a18d106c85e6254b61a137057de83
Kevin Qu <kevinqu88@gmail.com>
2025-10-07 15:01:39 -0400

    hi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    //dynamicIO: true,
+    //viewTransition: true,
+    useCache: true,
+    //ppr: 'incremental',
+    clientSegmentCache: true,
+    //turbopackPersistentCachingForBuild: true,
+    // turbopackPersistentCachingForDev: true,
+    cacheComponents: true,
+  },
+  reactCompiler: process.env.NODE_ENV === 'production' ? true : false,
+  // typedRoutes: true
 };
 
 export default nextConfig;
